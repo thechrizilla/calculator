5841f11d8df07558d5bdb8c939a6952fa2ba21ee
Chris Chan <chrisck.chan@hotmail.com>
2022-09-22 20:27:17 -0400

    add basic math operators and operate function

---
+++ app.js
@@ -0,0 +1,30 @@
+// basic math operator functions
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+
+// function that operats based on user input
+function operate(operator, a, b) {
+    if (operator === "+") {
+        return add(a, b);
+    } else if (operator === "-") {
+        return subtract(a, b);
+    } else if (operator === "×") {
+        return multiply(a, b);
+    } else if (operator === "÷") {
+        return divide(a, b);
+    }
+}
